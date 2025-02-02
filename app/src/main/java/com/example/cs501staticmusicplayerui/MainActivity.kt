c301d6d692c895b2207d649d9850b0c58ddc0db7
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-02-02 15:10:34 -0500

    added icons for play, shuffle, repeat, next, reverse and added them to a row component

--- app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
+++ app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cs501staticmusicplayerui.ui.theme.CS501StaticMusicPlayerUITheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CS501StaticMusicPlayerUITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MusicPlayer(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MusicPlayer(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(50.dp)
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.shuffle),
+            contentDescription = null,
+            modifier = modifier
+                .size(50.dp)
+                .padding(10.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.reverse),
+            contentDescription = null,
+            modifier = modifier
+                .size(50.dp)
+                .padding(10.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.play),
+            contentDescription = null,
+            modifier = modifier
+                .size(50.dp)
+                .padding(10.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.forward),
+            contentDescription = null,
+            modifier = modifier
+                .size(48.dp)
+                .padding(10.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.repeat),
+            contentDescription = null,
+            modifier = modifier
+                .size(50.dp)
+                .padding(10.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MusicPlayerPreview() {
     CS501StaticMusicPlayerUITheme {
-        Greeting("Android")
+        MusicPlayer()
     }
 }
