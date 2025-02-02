8929e312a3f0f8c7ba00110b9c48a4d06af412b1
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-02-02 15:43:36 -0500

    added artist name, song title, and horizontal divider to mimic the time bar on music player ui - placed everything into a card component

--- app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
+++ app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
@@ -18,8 +18,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cs501staticmusicplayerui.ui.theme.CS501StaticMusicPlayerUITheme
 
 class MainActivity : ComponentActivity() {
@@ -40,79 +42,113 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MusicPlayer(modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
+    Card(
+        modifier = modifier
+            .wrapContentSize()
+            .padding(5.dp)
+        ,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+            // changes the background color of card from default grey to white
+        )
     ) {
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White
-            )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(R.drawable.shuffle),
-                contentDescription = null,
-                modifier = modifier
-                    .size(30.dp)
+            Image( // album cover
+                painter = painterResource(R.drawable.album),
+                contentDescription = null
             )
-        }
 
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White
+            Text(
+                text = "BIRDS OF A FEATHER",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
             )
-        ) {
-            Image(
-                painter = painterResource(R.drawable.reverse),
-                contentDescription = null,
-                modifier = modifier
-                    .size(30.dp)
-            )
-        }
 
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White
+            Text(text = "Billie Eilish")
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth(0.60f)
+                    // trying to make the divider mimic a time bar
+                    .height(10.dp)
             )
-        ) {
-            Image(
-                painter = painterResource(R.drawable.play),
-                contentDescription = null,
-                modifier = modifier
-                    .size(30.dp)
-            )
-        }
 
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White
-            )
-        ) {
-            Image(
-                painter = painterResource(R.drawable.forward),
-                contentDescription = null,
-                modifier = modifier
-                    .size(28.dp)
-            )
-        }
+            Row( // row of all the buttons for music
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.shuffle),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(30.dp)
+                    )
+                }
 
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White
-            )
-        ) {
-            Image(
-                painter = painterResource(R.drawable.repeat),
-                contentDescription = null,
-                modifier = modifier
-                    .size(30.dp)
-            )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.reverse),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(30.dp)
+                    )
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.play),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(30.dp)
+                    )
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.forward),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(28.dp)
+                    )
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.repeat),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(30.dp)
+                    )
+                }
+            }
         }
     }
 }
