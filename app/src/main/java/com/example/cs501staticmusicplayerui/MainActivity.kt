72f34979bdeeb71d34d94fe5566dffde4a95fd4f
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-02-02 15:25:09 -0500

    created buttons for each icon and aligned them properly

--- app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
+++ app/src/main/java/com/example/cs501staticmusicplayerui/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,48 +43,77 @@ fun MusicPlayer(modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .padding(50.dp)
             .fillMaxWidth()
     ) {
-        Image(
-            painter = painterResource(R.drawable.shuffle),
-            contentDescription = null,
-            modifier = modifier
-                .size(50.dp)
-                .padding(10.dp)
-        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.shuffle),
+                contentDescription = null,
+                modifier = modifier
+                    .size(30.dp)
+            )
+        }
 
-        Image(
-            painter = painterResource(R.drawable.reverse),
-            contentDescription = null,
-            modifier = modifier
-                .size(50.dp)
-                .padding(10.dp)
-        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.reverse),
+                contentDescription = null,
+                modifier = modifier
+                    .size(30.dp)
+            )
+        }
 
-        Image(
-            painter = painterResource(R.drawable.play),
-            contentDescription = null,
-            modifier = modifier
-                .size(50.dp)
-                .padding(10.dp)
-        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.play),
+                contentDescription = null,
+                modifier = modifier
+                    .size(30.dp)
+            )
+        }
 
-        Image(
-            painter = painterResource(R.drawable.forward),
-            contentDescription = null,
-            modifier = modifier
-                .size(48.dp)
-                .padding(10.dp)
-        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.forward),
+                contentDescription = null,
+                modifier = modifier
+                    .size(28.dp)
+            )
+        }
 
-        Image(
-            painter = painterResource(R.drawable.repeat),
-            contentDescription = null,
-            modifier = modifier
-                .size(50.dp)
-                .padding(10.dp)
-        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.repeat),
+                contentDescription = null,
+                modifier = modifier
+                    .size(30.dp)
+            )
+        }
     }
 }
 
